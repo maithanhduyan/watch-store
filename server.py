6bd9a89dac81b59c80ad5acce3f04acf9041f2b3
Mai Thành Duy An <tiachop0102@gmail.com>
2022-10-26 15:37:45 +0900

    Create a template.

--- server.py
+++ server.py
@@ -2,9 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 
 @app.route('/')
 def index():
-    title = 'Socket Demo'
+    title = 'Watch Store'
     hello = 'Welcome to Watch Store'
-    return render_template('index.html', title=title, hello=hello)
+    return render_template('index.html', posts=posts)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
