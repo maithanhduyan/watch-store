f1ab70041f76b8037facaf9d07feb746aecc8401
Mai Thành Duy An <tiachop0102@gmail.com>
2022-10-27 10:00:54 +0900

    Register form UI.

--- server.py
+++ server.py
@@ -1,9 +1,35 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = '0b14f28c2309c942c4e3629e74307409'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 posts = [
     {
@@ -24,14 +50,11 @@ posts = [
 @app.route('/')
 @app.route('/home')
 def home():
-    title = 'Watch Store'
-    hello = 'Welcome to Watch Store'
+    user = User.username
     return render_template('index.html', posts=posts)
     
 @app.route('/about')
 def about():
-    title = 'Watch Store'
-    hello = 'Welcome to Watch Store'
     return render_template('about.html', posts=posts)
 
 @app.route("/register", methods=['GET', 'POST'])
